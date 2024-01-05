518898561ca239a603e7065a3847e4d6342bbaae
Asier Villar <asiervillar@outlook.es>
2024-01-05 15:39:55 +0100

    ADD: Cube solver server node

--- src/nodo_resolutor/src/nodo_resolutor/main.py
+++ src/nodo_resolutor/src/nodo_resolutor/main.py
@@ -4,27 +4,36 @@ import rospy
 import kociemba
 from std_msgs.msg import String
 
-rospy.init_node("primer_subscriber", anonymous=True)
 
-pub = rospy.Publisher('/cube_solved', String, queue_size=10)
+class CubeSolverServer:
+    def __init__(self):
+        rospy.init_node("primer_subscriber", anonymous=True)
 
-def recibir_mensajes(data: String) -> None:
-    cube_string = data.data 
+        self.publisher = rospy.Publisher('/cube_solved', String, queue_size=10)
+        self.subscriber = rospy.Subscriber('/cube_unsolved', String, self.get_message_callback) 
+
+    def get_message_callback(self, message: String) -> None:
+        cube_string = message.data 
+        try:
+            solution = kociemba.solve(cube_string)
+        except ValueError:
+            solution = None 
+            
+        is_valid = solution is not None
+
+        print("########### NUEVO CUBO ENVIADO ###########")
+        print("Config:", cube_string) 
+        print("Solucion:", solution)
+
+        if not rospy.is_shutdown() and is_valid:
+            self.publisher.publish(solution)
+        else:
+            self.publish("BAD CONFIG")
+
+
+if __name__ == "__main__":
     try:
-        solution = kociemba.solve(cube_string)
-    except ValueError:
-        solution = None 
-
-    print("Cubo recibido:", cube_string) 
-    print("La solución es:", solution)
-
-    is_valid = solution is not None
-
-    if rospy.is_shutdown() == False and is_valid:
-        pub.publish(solution)
-    else:
-        pub.publish("BAD CONFIG")
-
-rospy.Subscriber("/cube_unsolved", String, recibir_mensajes)
-
-rospy.spin()
+        node = CubeSolverServer()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
